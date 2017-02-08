4801b51e7ed1a86d62c04ae1e0489f73c1477b8f
Angel Junior <falecom@angeljunior.com.br>
2017-02-08 10:16:15 -0200

    initial

---
+++ README.md
@@ -0,0 +1,193 @@
+**Amazon Simple Email Service provides a simple way to send e-mails without having to maintain your own mail server.  Those PHP classes use the REST-based interface to that service.**
+
+
+****
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Basic Usage](#basic-usage)
+- [Recipients](#recipients)
+- [Message body](#message-body)
+- [Attachments](#attachments)
+- [Sending Bulk Messages](#sending-bulk-messages)
+- [API Endpoints](#api-endpoints)
+- [Helper Methods](#helper-methods)
+
+
+### Installation
+Install the latest version with
+
+	composer require angeljunior/aws-ses
+
+### Basic Usage
+
+```php
+<?php
+
+require_once 'vendor/autoload.php';
+
+$m = new SimpleEmailServiceMessage();
+$m->addTo('recipient@example.com');
+$m->setFrom('user@example.com');
+$m->setSubject('Hello, world!');
+$m->setMessageFromString('This is the message body.');
+
+$ses = new SimpleEmailService('AccessKey', 'SecretKey');
+print_r($ses->sendEmail($m));
+
+// Successful response should print something similar to:
+//Array(
+//     [MessageId] => 0000012dc5e4b4c0-b2c566ad-dcd0-4d23-bea5-f40da774033c-000000
+//     [RequestId] => 4953a96e-29d4-11e0-8907-21df9ed6ffe3
+//)
+
+```
+
+### Recipients
+
+```php
+<?php
+
+$m = new SimpleEmailServiceMessage();
+// Add many Recipients
+$m->addTo(array('dwight@example.com', 'angela@example.com'));
+
+// You can either add one by one or pass an array to 'To' and 'CC'
+$m->addCC('holly@example.com');
+$m->addCC(array('kelly@example.com', 'ryan@example.com'));
+
+// And 'BCC' and 'Reply-To' as well
+$m->addBCC('michael@example.com');
+$m->addBCC(array('kevin@example.com', 'oscar@example.com'));
+$m->addReplyTo('andy@example.com');
+$m->addReplyTo(array('stanley@example.com', 'erin@example.com'));
+
+
+// Also add names to any of the Recipients lists
+$m->addTo('Jim Carrey <jim@example.com>');
+
+```
+
+### Message body
+
+```php
+<?php
+
+// Additionally you can set the content of the email via:
+$m->setMessageFromFile('/path/to/some/file.txt');
+$m->setMessageFromURL('http://example.com/somefile.txt');
+
+// And have both Text and HTML version with:
+$m->setMessageFromString($text, $html);
+$m->setMessageFromFile($textfilepath, $htmlfilepath);
+$m->setMessageFromURL($texturl, $htmlurl);
+$m->setMessageFromHTMLString($html);
+
+// Remember that setMessageFromString, setMessageFromFile, and setMessageFromURL are mutually exclusive.
+// If you call more than one, then whichever call you make last will be the message used.
+
+// You can also set the encoding of the Subject and the Message Body
+$m->setSubjectCharset('ISO-8859-1');
+$m->setMessageCharset('ISO-8859-1');
+
+```
+The default is UTF-8 if you do not specify a charset, which is usually the right setting. You can read more information in the [SES API documentation](http://docs.amazonwebservices.com/ses/latest/APIReference/API_Content.html)
+
+### Attachments
+
+```php
+<?php
+
+$m->addAttachmentFromData('my_text_file.txt', 'Simple content', 'text/plain');
+$m->addAttachmentFromFile('my_PFD_file.pdf', '/path/to/pdf/file', 'application/pdf');
+
+// SendRawEmail is explicitly used when there are attachments:
+$ses->sendEmail($m);
+// Sending raw email can be enforsed with:
+$ses->sendEmail($m, $use_raw_request = true);
+
+// Now you can add an inline file in the message
+$m->addAttachmentFromFile('logo.png','path/to/logo.png','application/octet-stream', '<logo.png>' , 'inline');
+// and use it in the html version of the e-mail: <img src='cid:logo.png' />
+
+```
+
+### Sending Bulk Messages
+When hundreds of emails have to be sent in bulk it's best to use the Bulk mode which essentially reuses a CURL handler and reduces the number of SSL handshakes and this gives a better performance.
+
+```php
+<?php
+
+// Enable bulk sending mode (reuse of CURL handler)
+$ses->setBulkMode(true);
+
+// Send the messages
+foreach($messages as $message) {
+	$ses->sendEmail($message);
+}
+
+// Disable bulk sending mode
+$ses->setBulkMode(false);
+```
+
+### API Endpoints
+Few [Regions and Amazon SES endpoints](http://docs.aws.amazon.com/ses/latest/DeveloperGuide/regions.html) are available and they can be used like this:
+
+```php
+<?php
+
+$region_endpoint = SimpleEmailService::AWS_US_EAST_1;
+$ses = new SimpleEmailService('AccessKey', 'SecretKey', $region_endpoint);
+```
+
+### Helper Methods
+
+```php
+<?php
+
+// Get the addresses that have been verified in your AWS SES account
+$ses->listVerifiedEmailAddresses();
+// Delete a verified address
+$ses->deleteVerifiedEmailAddress('user@example.com');
+// Send a confirmation email in order to verify a new email
+$ses->verifyEmailAddress('user@example.com');
+
+// Get Send Quota
+$ses->getSendQuota();
+// Get Send Statistics
+$ses->getSendStatistics()
+
+```
+See the documentation on [GetSendQuota](http://docs.amazonwebservices.com/ses/latest/APIReference/API_GetSendQuota.html) and [GetSendStatistics](http://docs.amazonwebservices.com/ses/latest/APIReference/API_GetSendStatistics.html) for more information on these calls.
+
+### Errors
+By default when Amazon SES API returns an error it will be triggered with [`trigger_error`](http://php.net/manual/en/function.trigger-error.php):
+
+```php
+<?php
+
+// Set the default behaviour for handling errors
+$trigger_error = true;
+$ses = new SimpleEmailService('AccessKey', 'SecretKey', $region_endpoint, $trigger_error);
+
+// Or overwrite the main setting on a single call
+$use_raw_request = false;
+$trigger_error = false;
+$ses->sendEmail($m, $use_raw_request, $trigger_error);
+
+```
+
+
+### Changelog
+v.1.0.0.
+
+ - Inital impport
+
+### Todo List
+
+ - Fully document the class methods with phpdoc tags
+ - Build documentation with phpDocumentor
+ - Move examples to files
+ - Make a [Composer](https://packagist.org/) package
+ - Allow X-Headers usage
---
+++ src/SimpleEmailServiceRequest.php
@@ -0,0 +1,254 @@
+<?php
+/**
+* SimpleEmailServiceRequest PHP class
+*
+* @link https://github.com/angeljunior/aws-ses
+* @package AmazonSimpleEmailService
+* @version v1.0.0
+*/
+final class SimpleEmailServiceRequest
+{
+	private $ses, $verb, $parameters = array();
+
+	// CURL request handler that can be reused
+	protected $curl_handler = null;
+
+	// Holds the response from calling AWS's API
+	protected $response;
+
+	//
+	public static $curlOptions = array();
+
+	/**
+	* Constructor
+	*
+	* @param string $ses The SimpleEmailService object making this request
+	* @param string $verb HTTP verb
+	* @return void
+	*/
+	public function __construct($ses, $verb = 'GET') {
+		$this->ses = $ses;
+		$this->verb = $verb;
+		$this->response = (object) array('body' => '', 'code' => 0, 'error' => false);
+	}
+
+	/**
+	* Set HTTP method
+	*
+	* @return SimpleEmailServiceRequest $this
+	*/
+	public function setVerb($verb) {
+		$this->verb = $verb;
+		return $this;
+	}
+
+	/**
+	* Set request parameter
+	*
+	* @param string  $key Key
+	* @param string  $value Value
+	* @param boolean $replace Whether to replace the key if it already exists (default true)
+	* @return SimpleEmailServiceRequest $this
+	*/
+	public function setParameter($key, $value, $replace = true) {
+		if(!$replace && isset($this->parameters[$key])) {
+			$temp = (array)($this->parameters[$key]);
+			$temp[] = $value;
+			$this->parameters[$key] = $temp;
+		} else {
+			$this->parameters[$key] = $value;
+		}
+
+		return $this;
+	}
+
+	/**
+	* Get the params for the reques
+	*
+	* @return array $params
+	*/
+	public function getParametersEncoded() {
+		$params = array();
+
+		foreach ($this->parameters as $var => $value) {
+			if(is_array($value)) {
+				foreach($value as $v) {
+					$params[] = $var.'='.$this->__customUrlEncode($v);
+				}
+			} else {
+				$params[] = $var.'='.$this->__customUrlEncode($value);
+			}
+		}
+
+		sort($params, SORT_STRING);
+
+		return $params;
+	}
+
+	/**
+	* Clear the request parameters
+	* @return SimpleEmailServiceRequest $this
+	*/
+	public function clearParameters() {
+		$this->parameters = array();
+		return $this;
+	}
+
+	/**
+	* Instantiate and setup CURL handler for sending requests.
+	* Instance is cashed in `$this->curl_handler`
+	*
+	* @return resource $curl_handler
+	*/
+	protected function getCurlHandler() {
+		if (!empty($this->curl_handler))
+			return $this->curl_handler;
+
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_USERAGENT, 'SimpleEmailService/php');
+
+		curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, ($this->ses->verifyHost() ? 2 : 0));
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, ($this->ses->verifyPeer() ? 1 : 0));
+		curl_setopt($curl, CURLOPT_HEADER, false);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, false);
+		curl_setopt($curl, CURLOPT_WRITEFUNCTION, array(&$this, '__responseWriteCallback'));
+		curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+
+		foreach(self::$curlOptions as $option => $value) {
+			curl_setopt($curl, $option, $value);
+		}
+
+		$this->curl_handler = $curl;
+
+		return $this->curl_handler;
+	}
+
+	/**
+	* Get the response
+	*
+	* @return object | false
+	*/
+	public function getResponse() {
+
+		// must be in format 'Sun, 06 Nov 1994 08:49:37 GMT'
+		$date = gmdate('D, d M Y H:i:s e');
+		$query = implode('&', $this->getParametersEncoded());
+		$auth = 'AWS3-HTTPS AWSAccessKeyId='.$this->ses->getAccessKey();
+		$auth .= ',Algorithm=HmacSHA256,Signature='.$this->__getSignature($date);
+		$url = 'https://'.$this->ses->getHost().'/';
+
+		$headers = array();
+		$headers[] = 'Date: ' . $date;
+		$headers[] = 'Host: ' . $this->ses->getHost();
+		$headers[] = 'X-Amzn-Authorization: ' . $auth;
+
+		$curl_handler = $this->getCurlHandler();
+		curl_setopt($curl_handler, CURLOPT_CUSTOMREQUEST, $this->verb);
+
+		// Request types
+		switch ($this->verb) {
+			case 'GET':
+			case 'DELETE':
+				$url .= '?'.$query;
+				break;
+
+			case 'POST':
+				curl_setopt($curl_handler, CURLOPT_POSTFIELDS, $query);
+				$headers[] = 'Content-Type: application/x-www-form-urlencoded';
+				break;
+		}
+		curl_setopt($curl_handler, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($curl_handler, CURLOPT_URL, $url);
+
+
+		// Execute, grab errors
+		if (curl_exec($curl_handler)) {
+			$this->response->code = curl_getinfo($curl_handler, CURLINFO_HTTP_CODE);
+		} else {
+			$this->response->error = array(
+				'curl' => true,
+				'code' => curl_errno($curl_handler),
+				'message' => curl_error($curl_handler),
+			);
+		}
+
+		// cleanup for reusing the current instance for multiple requests
+		curl_setopt($curl_handler, CURLOPT_POSTFIELDS, '');
+		$this->parameters = array();
+
+		// Parse body into XML
+		if ($this->response->error === false && !empty($this->response->body)) {
+			$this->response->body = simplexml_load_string($this->response->body);
+
+			// Grab SES errors
+			if (!in_array($this->response->code, array(200, 201, 202, 204))
+				&& isset($this->response->body->Error)) {
+				$error = $this->response->body->Error;
+				$output = array();
+				$output['curl'] = false;
+				$output['Error'] = array();
+				$output['Error']['Type'] = (string)$error->Type;
+				$output['Error']['Code'] = (string)$error->Code;
+				$output['Error']['Message'] = (string)$error->Message;
+				$output['RequestId'] = (string)$this->response->body->RequestId;
+
+				$this->response->error = $output;
+				unset($this->response->body);
+			}
+		}
+
+		$response = $this->response;
+		$this->response = (object) array('body' => '', 'code' => 0, 'error' => false);
+
+		return $response;
+	}
+
+	/**
+	* Destroy any leftover handlers
+	*/
+	public function __destruct() {
+		if (!empty($this->curl_handler))
+			@curl_close($this->curl_handler);
+	}
+
+	/**
+	* CURL write callback
+	*
+	* @param resource $curl CURL resource
+	* @param string $data Data
+	* @return integer
+	*/
+	private function __responseWriteCallback(&$curl, &$data) {
+		if (!isset($this->response->body)) {
+			$this->response->body = $data;
+		} else {
+			$this->response->body .= $data;
+		}
+
+		return strlen($data);
+	}
+
+	/**
+	* Contributed by afx114
+	* URL encode the parameters as per http://docs.amazonwebservices.com/AWSECommerceService/latest/DG/index.html?Query_QueryAuth.html
+	* PHP's rawurlencode() follows RFC 1738, not RFC 3986 as required by Amazon. The only difference is the tilde (~), so convert it back after rawurlencode
+	* See: http://www.morganney.com/blog/API/AWS-Product-Advertising-API-Requires-a-Signed-Request.php
+	*
+	* @param string $var String to encode
+	* @return string
+	*/
+	private function __customUrlEncode($var) {
+		return str_replace('%7E', '~', rawurlencode($var));
+	}
+
+	/**
+	* Generate the auth string using Hmac-SHA256
+	*
+	* @internal Used by SimpleDBRequest::getResponse()
+	* @param string $string String to sign
+	* @return string
+	*/
+	private function __getSignature($string) {
+		return base64_encode(hash_hmac('sha256', $string, $this->ses->getSecretKey(), true));
+	}
+}
